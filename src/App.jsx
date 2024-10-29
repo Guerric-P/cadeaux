365d52724ab493fbb4c3e2265fc0f21c830cdeea
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2024-10-29 23:58:18 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+import './App.css';
+
+function Cadeaux() {
+  const [membres, setMembres] = useState([
+    {
+      nom: 'Guerric',
+      famille: 'Prunet',
+    },
+    {
+      nom: 'Hélène',
+      famille: 'Prunet',
+    },
+    {
+      nom: 'Gémo',
+      famille: 'Gémo',
+    },
+    {
+      nom: 'Padre',
+      famille: 'Padre',
+    },
+    {
+      nom: 'Madre',
+      famille: 'Madre',
+    },
+    {
+      nom: 'Sébastien',
+      famille: 'Garcia',
+    },
+    {
+      nom: 'Nis',
+      famille: 'Garcia',
+    },
+  ]);
+  const [resultat, setResultat] = useState([]);
+
+  function compute() {
+    const resultat = [];
+
+    for (const membre of membres) {
+      const eligibles = membres.filter(
+        (x) =>
+          x.famille !== membre.famille && !resultat.some((r) => r.donneur === x)
+      );
+      const elu = eligibles[Math.floor(Math.random() * eligibles.length)];
+
+      if (elu) resultat.push({ donneur: elu, receveur: membre });
+    }
+
+    if (resultat.length === membres.length) {
+      setResultat(resultat);
+    } else {
+      compute();
+    }
+  }
+
+  return (
+    <>
+      <table>
+        <caption>Cadeaux</caption>
+        <thead>
+          <tr>
+            <th scope="col">Nom</th>
+            <th scope="col">Famille</th>
+          </tr>
+        </thead>
+        <tbody>
+          {membres.map((m, i) => (
+            <tr key={i}>
+              <td>
+                <input
+                  value={m.nom}
+                  onChange={(event) => {
+                    const newMembres = [...membres];
+                    newMembres[i].nom = event.target.value;
+                    setMembres(newMembres);
+                  }}
+                />
+              </td>
+              <td>
+                <input
+                  value={m.famille}
+                  onChange={(event) => {
+                    const newMembres = [...membres];
+                    newMembres[i].famille = event.target.value;
+                    setMembres(newMembres);
+                  }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th scope="row" colSpan={2}>
+              <button
+                onClick={() =>
+                  setMembres([...membres, { nom: '', famille: '' }])
+                }
+              >
+                Ajouter un membre
+              </button>
+            </th>
+            <button onClick={compute}>Calculer</button>
+          </tr>
+        </tfoot>
+      </table>
+
+      <table>
+        <caption>Résultat</caption>
+
+        <thead>
+          <tr>
+            <th scope="col">Donneur</th>
+            <th scope="col">Receveur</th>
+          </tr>
+        </thead>
+
+        {resultat.map((r, i) => (
+          <tr key={i}>
+            <td>{r.donneur.nom}</td>
+            <td>{r.receveur.nom}</td>
+          </tr>
+        ))}
+      </table>
+    </>
+  );
+}
+
+export default Cadeaux;
