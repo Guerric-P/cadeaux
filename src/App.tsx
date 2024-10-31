25aefbe19bd8ce44efc5b2f4734da8487bb22989
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2024-10-31 23:42:04 +0100

    organize code

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react';
 import { Membre } from './model/membre';
 import { GraphNode } from './model/graph-node';
+import { shuffleArray } from './util/array';
 
 function Cadeaux() {
   const [membres, setMembres] = useState<Membre[]>([
@@ -52,13 +53,6 @@ function Cadeaux() {
     } else {
       setResultat([]);
       setErreur(true);
-    }
-  }
-
-  function shuffleArray(array: any[]) {
-    for (let i = array.length - 1; i >= 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
     }
   }
 
