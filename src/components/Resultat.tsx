17b67bc8036c43de006f5456a5e6d51f37f2fdf3
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2024-11-30 19:50:33 +0100

    format resultats

--- src/components/Resultat.tsx
+++ src/components/Resultat.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { Membre } from '../model/membre';
 
 export const Resultat = (props: { titre: string, resultat: { donneur: Membre, receveur: Membre }[] }) =>
-    props.resultat && <table style={{ width: '100%' }}>
-        <caption>{props.titre}</caption>
+    props.resultat && <table className="min-w-full table-auto">
+        <caption className="text-lg font-bold py-2">{props.titre}</caption>
         {
             props.resultat.length ?
                 [
                     <thead>
                         <tr>
-                            <th scope="col">Donneur</th>
-                            <th scope="col">Receveur</th>
+                            <th className="px-4 py-2 text-left" scope="col">Donneur</th>
+                            <th className="px-4 py-2 text-left" scope="col">Receveur</th>
                         </tr>
                     </thead>,
                     props.resultat.map((r, i) => (
-                        <tr key={i}>
-                            <td>{r.donneur.nom}</td>
-                            <td>{r.receveur.nom}</td>
+                        <tr className="border-t" key={i}>
+                            <td className="text-left sm:px-4 sm:py-2 p-1">{r.donneur.nom}</td>
+                            <td className="text-left sm:px-4 sm:py-2 p-1">{r.receveur.nom}</td>
                         </tr>
                     ))
                 ] : <div>Aucune répartition n'existe pour les contraintes données.</div>
