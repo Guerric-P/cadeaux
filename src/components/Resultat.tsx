34115209a3a4ed71b45f563d0f0244a735f63dad
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2024-11-30 20:06:35 +0100

    text align

--- src/components/Resultat.tsx
+++ src/components/Resultat.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Membre } from '../model/membre';
 
 export const Resultat = (props: { titre: string, resultat: { donneur: Membre, receveur: Membre }[] }) =>
-    props.resultat && <div className="min-w-full">
+    props.resultat && <div className="w-fit m-auto text-left">
         <div className="text-lg font-bold py-2">{props.titre}</div>
         {
             props.resultat.length ?
                 props.resultat.map((r, i) => (
                     <div key={i}>
-                        <span className="text-left sm:px-4 sm:py-2 p-1">{r.donneur.nom}</span>
-                        donne un cadeau 🎁 à
-                        <span className="text-left sm:px-4 sm:py-2 p-1">{r.receveur.nom}</span>
+                        <span className="font-bold">{r.donneur.nom}</span>
+                        &nbsp;donne un cadeau 🎁 à&nbsp;
+                        <span className="font-bold">{r.receveur.nom}</span>
                     </div>
                 ))
                 : <div>Aucune répartition n'existe pour les contraintes données.</div>
