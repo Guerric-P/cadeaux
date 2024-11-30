111a524279c52cf52641198f88cc321fc419696e
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2024-11-30 22:40:56 +0100

    fix favicon

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import legacy from '@vitejs/plugin-legacy'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), legacy({ targets: ['defaults', 'not IE 11'] })],
-  base: 'cadeaux'
+  base: '/cadeaux'
 })
