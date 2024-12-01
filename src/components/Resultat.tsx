9a00ca9fe54825a16ea19789c80252eafa1a0a30
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2024-12-01 15:07:04 +0100

    syntax fix

--- src/components/Resultat.tsx
+++ src/components/Resultat.tsx
@@ -5,15 +5,14 @@ export const Resultat = (props: { titre: string, resultat: { donneur: Membre, re
     props.resultat && <div className="w-fit text-left">
         <div className="text-lg font-bold py-2">{props.titre}</div>
         {
-            props.resultat.length ?
-                props.resultat.map((r, i) => (
+            props.resultat.length
+                ? props.resultat.map((r, i) =>
                     <div key={i}>
                         <span className="font-bold">{r.donneur.nom}</span>
                         &nbsp;donne un cadeau 🎁 à&nbsp;
                         <span className="font-bold">{r.receveur.nom}</span>
                     </div>
-                ))
+                )
                 : <div>Aucune répartition n'existe pour les contraintes données.</div>
         }
-
     </div>
