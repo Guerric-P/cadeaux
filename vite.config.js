9fe7450d7fb8d6500514f892894012536c005e9b
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2024-12-29 16:59:03 +0100

    add vite tsconfig paths plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy({ targets: ['defaults', 'not IE 11'] })],
-  base: '/cadeaux'
+  plugins: [react(), tsconfigPaths(), legacy({ targets: ['defaults', 'not IE 11'] })],
+  base: '/cadeaux',
 })
